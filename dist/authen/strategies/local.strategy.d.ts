65f1af5def99054bef67b2b714e55f0a7742253e
bielzd <gabriel_2131@live.com>
2023-06-27 22:45:12 -0300

    commit

--- dist/authen/strategies/local.strategy.d.ts
+++ dist/authen/strategies/local.strategy.d.ts
@@ -4,6 +4,6 @@ declare const LocalStrategy_base: new (...args: any[]) => Strategy;
 export declare class LocalStrategy extends LocalStrategy_base {
     private authService;
     constructor(authService: AuthenService);
-    validate(email: string, password: string): Promise<import("../../empresario/entities/empresario.entity").Empresario | import("../../candidato/entities/candidato.entity").Candidato>;
+    validate(email: string, password: string): Promise<import("../../candidato/entities/candidato.entity").Candidato | import("../../empresario/entities/empresario.entity").Empresario>;
 }
 export {};
